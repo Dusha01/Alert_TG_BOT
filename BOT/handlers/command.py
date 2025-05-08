dd87aa8430f06d229ace32fc25306b0f88a1e6f8
Dusha_01 <87571857+Dusha01@users.noreply.github.com>
2025-05-08 10:17:14 +1000

    +url prometheuse

--- BOT/handlers/command.py
+++ BOT/handlers/command.py
@@ -6,10 +6,22 @@ from config.setting import Setting
 
 router = Router()
 
+
+
 @router.message(Command(commands=['grafana']))
 async def send_url_grafana(msg: Message):
     user_id = msg.from_user.id
     if user_id == int(Setting.USER_ID_1) or user_id == int(Setting.USER_ID_2):
         await msg.answer(f"{Setting.GRAFANA_URL}")
+    else:
+        await msg.answer("Послан")
+
+
+
+@router.message(Command(commands=['Prometheus']))
+async def send_url_prometheus(msg: Message):
+    user_id = msg.from_user.id
+    if user_id == int(Setting.USER_ID_1) or user_id == int(Setting.USER_ID_2):
+        await msg.answer(f"{Setting.PROMETHEUS_URL}")
     else:
         await msg.answer("Послан")
